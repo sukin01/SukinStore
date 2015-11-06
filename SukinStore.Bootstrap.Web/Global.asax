7c4003ed20aeb2625fef6ea90bee7571847dc8e2
sukin01 <sukin@outlook.com>
2015-11-06 18:13:00 +0800

    添加bootstrap练习项目

---
+++ SukinStore.Bootstrap.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SukinStore.Bootstrap.Web.MvcApplication" Language="C#" %>
---
+++ SukinStore.Bootstrap.Web/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(SukinStore.Bootstrap.Web.Startup))]
+namespace SukinStore.Bootstrap.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
